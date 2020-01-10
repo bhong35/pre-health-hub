06581631a0762c472506b84e255ec6d363073973
Benjamin Hong <benjaminhong@live.com>
2020-01-10 09:40:16 -0600

    styled flashcard error screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Image, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default function LinksScreen() {
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <ExpoLinksView />
+       <View style={styles.text}>
+            <Image source={require('../assets/images/booboo.png')} style={styles.image} />
+            <Text style={styles.comingSoon}>This functionality is coming soon!</Text>
+            <Text style={styles.thanks}>Thanks for your patience :)</Text>
+       </View>
     </ScrollView>
   );
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  title: 'Flashcards',
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     paddingTop: 15,
     backgroundColor: 'rgba(255, 99, 71, 0.15)',
   },
+  text: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  image: {
+      width: 50,
+      height: 50,
+      marginBottom: 20
+  },
+  comingSoon: {
+  },
+  thanks: {
+  }
 });
